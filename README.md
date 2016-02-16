840ef5493bbd1085c04a154fd1ac76e1dd5da0ff
elf Pavlik <perpetual-tripper@wwelves.org>
2016-02-15 23:36:01 -0600

    verify & publish

--- README.md
+++ README.md
@@ -4,9 +4,10 @@
 
 * [x] generate key pair
 * [x] sign data
+* [x] verify signed data
 * [x] fetch single resource
+* [x] publish data to workspace (we start with adding a resource to a concrete container)
 * [ ] fetch identity and all related containers ( see o-data in o-app-karma - eager )
-* [ ] save data to pod (we start with adding a resource to a concrete container)
 
 ## usage
 
@@ -22,10 +23,22 @@ gulp key:new
 gulp sign --path /path/to/file.jsonld --privPem /path/to/key.pem --pubKey https://bob.example/keys/1
 ```
 
+### verify
+
+```
+gulp verify --path /path/to/file.jsonld
+```
+
 ### fetch
 
 ```
 gulp fetch --uri https://alice.example/
+```
+
+### publish
+
+```
+gulp publish --path /path/o/file.jsonld --container https://bob.example/activities
 ```
 
 ## dependencies
@@ -37,7 +50,6 @@ gulp fetch --uri https://alice.example/
 ## common
 * gulp
 * yargs
-* lodash
 * rdf-ext
  * jsonld
  * n3
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -15,30 +15,80 @@ gulp.task('key:new', () => {
   console.log(forge.pki.publicKeyToPem(keypair.publicKey))
 })
 
-/*
- * prints document as expanded JSON-LD
+/**
+ * @param path - path to JSON-LD file we want to sign
+ * @param pubKey - URI of a public key
+ * @param privPem - PEM encoded private key
  */
-gulp.task('sign', () => {
+gulp.task('sign', (done) => {
   console.log('sign path: ', argv.path)
   console.log('sign pubKey: ', argv.pubKey)
   console.log('sign privPem: ', argv.privPem)
   let doc = JSON.parse(fs.readFileSync(argv.path, 'utf8'))
   let privPem = fs.readFileSync(argv.privPem, 'utf8')
-  let client = new Client(privPem, argv.pubKey)
+  let client = new Client(fetch, privPem, argv.pubKey)
   client.sign(doc)
     .then((signed) => {
-      return console.log(JSON.stringify(signed))
+      console.log(JSON.stringify(signed))
+      return done()
     }).catch((err) => {
       console.log(err)
+      return done()
     })
 })
 
-gulp.task('fetch', () => {
+/**
+ * @param path - path to JSON-LD file we want to verify
+ */
+gulp.task('verify', (done) => {
+  console.log('sign path: ', argv.path)
+  let doc = JSON.parse(fs.readFileSync(argv.path, 'utf8'))
+  let client = new Client(fetch)
+  client.verify(doc)
+    .then((verified) => {
+      console.log(verified)
+      return done()
+    }).catch((err) => {
+      console.log(err)
+      return done()
+    })
+})
+
+/**
+ * @param uri - URI to fetch
+ */
+
+gulp.task('fetch', (done) => {
   console.log('fetch uri: ', argv.uri)
   fetch(argv.uri)
     .then((res) => {
       return res.text()
     }).then((body) => {
       console.log(body)
+      return done()
+    }).catch((err) => {
+      console.log(err)
+      return done()
+    })
+})
+
+/**
+ * @param path - path to JSON-LD file
+ * @param container - uri of container we post to
+ */
+
+gulp.task('publish', (done) => {
+  console.log('publish path: ', argv.path)
+  console.log('publish container: ', argv.container)
+  let doc = JSON.parse(fs.readFileSync(argv.path, 'utf8'))
+  fetch(argv.container, { method: 'POST', body: JSON.stringify(doc), headers: { "Content-Type": "application/ld+json" } })
+    .then(function(res) {
+      return res.json();
+    }).then(function(json) {
+      console.log(json);
+      return done()
+    }).catch((err) => {
+      console.log(err)
+      return done()
     })
 })
